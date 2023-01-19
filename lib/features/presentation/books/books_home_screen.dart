56fcaf53a318a1fca9d83fec0df371db408ff98c
hiroyuki29 <hiroyuki0209@gmail.com>
2023-01-19 12:47:47 +0900

    [update] book検索一覧追加

--- lib/features/presentation/books/books_home_screen.dart
+++ lib/features/presentation/books/books_home_screen.dart
@@ -1,12 +1,60 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:spimo/features/domain/model/book/book.dart';
+import 'package:spimo/features/domain/repository/books_repository.dart';
 
-class BooksHomeScreen extends StatelessWidget {
+class BooksHomeScreen extends HookConsumerWidget {
   const BooksHomeScreen({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    return const SafeArea(
-      child: Text('books'),
+  Widget build(BuildContext context, WidgetRef ref) {
+    final searchWord = useTextEditingController(text: '');
+    final books = useState<List<Book>>([]);
+    final book = useState<Book?>(null);
+
+    Future<void> fetchBooks() async {
+      final searchedBookList =
+          await ref.read(booksProvider).getBookList(searchWord.text);
+      books.value = searchedBookList;
+      final id = books.value[5].id;
+      book.value = await ref.read(booksProvider).getBook(id);
+    }
+
+    return Scaffold(
+      body: SafeArea(
+        child: Column(
+          children: [
+            TextFormField(
+              controller: searchWord,
+            ),
+            TextButton(
+              onPressed: () {
+                fetchBooks();
+              },
+              child: const Text('click'),
+            ),
+            books.value.isEmpty || book.value == null
+                ? const Text('データなし')
+                : SingleChildScrollView(
+                    child: ListView.builder(
+                      shrinkWrap: true,
+                      itemCount: books.value.length,
+                      itemBuilder: (context, index) {
+                        return Column(
+                          children: [
+                            // if (book.value!.imageLinks != '')
+                            //   Image.network(book.value!.imageLinks!),
+                            Text(books.value[index].title.toString()),
+                            Text(books.value[index].authors.toString()),
+                          ],
+                        );
+                      },
+                    ),
+                  ),
+          ],
+        ),
+      ),
     );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -2,8 +2,6 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
-import 'package:speech_to_text/speech_recognition_result.dart';
-import 'package:speech_to_text/speech_to_text.dart';
 import 'package:spimo/firebase_options.dart';
 import 'package:spimo/routing/app_router.dart';
 
@@ -43,99 +41,7 @@ class MyApp extends ConsumerWidget {
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key}) : super(key: key);
 
-  @override
-  MyHomePageState createState() => MyHomePageState();
-}
-
-class MyHomePageState extends State<MyHomePage> {
-  final SpeechToText _speechToText = SpeechToText();
-  bool _speechEnabled = false;
-  String _lastWords = '';
-  List<String> wordList = [];
-  String _currentLocaleId = '';
-
-  @override
-  void initState() {
-    super.initState();
-    _initSpeech();
-  }
-
-  void _initSpeech() async {
-    _speechEnabled = await _speechToText.initialize();
-    var systemLocale = await _speechToText.systemLocale();
-    _currentLocaleId = systemLocale?.localeId ?? '';
-    setState(() {});
-  }
-
-  void _startListening() async {
-    await _speechToText.listen(
-      onResult: _onSpeechResult,
-      localeId: _currentLocaleId,
-    );
-    setState(() {});
-  }
-
-  void _stopListening() async {
-    await _speechToText.stop();
-    wordList.add(_lastWords);
-    setState(() {});
-  }
-
-  void _onSpeechResult(SpeechRecognitionResult result) {
-    setState(() {
-      _lastWords = result.recognizedWords;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Speech Demo'),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Container(
-              padding: const EdgeInsets.all(16),
-              child: const Text(
-                'Recognized words:',
-                style: TextStyle(fontSize: 20.0),
-              ),
-            ),
-            Container(
-              padding: const EdgeInsets.all(16),
-              child: Text(
-                _speechToText.isListening
-                    ? _lastWords
-                    : _speechEnabled
-                        ? 'Tap the microphone to start listening...'
-                        : 'Speech not available',
-              ),
-            ),
-            ListView.builder(
-              shrinkWrap: true,
-              itemCount: wordList.length,
-              itemBuilder: (context, index) {
-                return Text(wordList[index]);
-              },
-            )
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed:
-            _speechToText.isNotListening ? _startListening : _stopListening,
-        tooltip: 'Listen',
-        child: Icon(_speechToText.isNotListening ? Icons.mic_off : Icons.mic),
-      ),
-    );
-  }
-}
 
 
 
